4feecb65d790d0e05f27fce8233fe325cbbc4fd7
Benzir <benzir.ahmed7@gmail.com>
2015-02-26 15:55:47 -0500

    created base GiantPlanet code

--- Assignment03_Abstract Planets/GiantPlanet.cs
+++ Assignment03_Abstract Planets/GiantPlanet.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Assignment03_Abstract_Planets
 {
-    class GiantPlanet
+    class GiantPlanet : Planet
     {
+        private int _type;
+    
+        public void giantPlant(string name, double diameter, double mass, string type)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool HasMoons()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool HasRings()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
